278d0ba29750e6e25ab0e1ad30ec6a562ae9d14f
raviojha96 <raviojha500@gmail.com>
2024-05-01 19:02:26 +0530

    routes creates

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,25 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProductsComponent } from './components/products/products.component';
+import { CustomersComponent } from './components/customers/customers.component';
+import { OrdersComponent } from './components/orders/orders.component';
+import { InventoryComponent } from './components/inventory/inventory.component';
+import { SalesComponent } from './components/sales/sales.component';
 
 export const routes: Routes = [
-    {path : '' , component:LoginComponent},
-    {path : 'dashboard' , component:DashboardComponent}
+  { path: '', component: LoginComponent },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    children: [
+      { path: '', component: HomeComponent },
+      { path: 'products', component: ProductsComponent },
+      { path: 'customers', component: CustomersComponent },
+      { path: 'orders', component: OrdersComponent },
+      { path: 'inventory', component: InventoryComponent },
+      { path: 'sales', component: SalesComponent },
+    ],
+  },
 ];
- 
